fba20206bbb50633dadbc2999a83bf26b779ff98
Nate Spring <45617450+NateSpring@users.noreply.github.com>
2019-07-28 09:35:06 -0700

    Add files via upload
    7/28/19: Resized header images for faster loading. One image was reduced from 4.7mb to 117kb.

---
+++ build/precache-manifest.0f2fd308e3f01b973c9cd60222555e7a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ad3cf1aeb593443823ff53d6ebe026",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a61e73812c29fa3d36c9",
+    "url": "/static/css/main.a0d3a403.chunk.css"
+  },
+  {
+    "revision": "61433e735fbb2d010e13",
+    "url": "/static/js/2.b7f52e28.chunk.js"
+  },
+  {
+    "revision": "a61e73812c29fa3d36c9",
+    "url": "/static/js/main.dad3b396.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e925438b69715c49c2505a73d3fe8773",
+    "url": "/static/media/lander.e925438b.png"
+  },
+  {
+    "revision": "82987827f2526914f03f1488ebda55a2",
+    "url": "/static/media/nasalization-rg.82987827.ttf"
+  },
+  {
+    "revision": "5b05d0597c661e0a6fbfd01123ca7439",
+    "url": "/static/media/rover.5b05d059.png"
+  }
+]);
