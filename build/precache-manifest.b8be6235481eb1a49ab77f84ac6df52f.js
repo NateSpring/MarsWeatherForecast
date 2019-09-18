0205d4357a60dbcaebf4dd219e72e035d85f2112
Nate Spring <45617450+NateSpring@users.noreply.github.com>
2019-09-18 06:58:05 -0700

    Add files via upload
    9/18/19: Repaired incoming JSON data.

---
+++ build/precache-manifest.b8be6235481eb1a49ab77f84ac6df52f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a60e4cb249b82549262404aea84879",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82903a7a8e7d685a4816",
+    "url": "/static/css/main.1fab4e7e.chunk.css"
+  },
+  {
+    "revision": "61433e735fbb2d010e13",
+    "url": "/static/js/2.b7f52e28.chunk.js"
+  },
+  {
+    "revision": "82903a7a8e7d685a4816",
+    "url": "/static/js/main.19781c21.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e925438b69715c49c2505a73d3fe8773",
+    "url": "/static/media/lander.e925438b.png"
+  },
+  {
+    "revision": "82987827f2526914f03f1488ebda55a2",
+    "url": "/static/media/nasalization-rg.82987827.ttf"
+  },
+  {
+    "revision": "5b05d0597c661e0a6fbfd01123ca7439",
+    "url": "/static/media/rover.5b05d059.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b3bfb1c567c80e8502d7e15918baba65.js"
+  "/precache-manifest.b8be6235481eb1a49ab77f84ac6df52f.js"
 );
 
 self.addEventListener('message', (event) => {
